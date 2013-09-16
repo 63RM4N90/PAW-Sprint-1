61aa145e764b1bab46402e7857de8f7b81333ccf
YOUR NAME <YOUR EMAIL>
2013-09-16 13:20:12 -0300

    ahora en la pagina de hashtags se muestran los comentarios preprocesados. Se removio la clase Image...

--- Sprint1/src/main/java/controller/HashtagPage.java
+++ Sprint1/src/main/java/controller/HashtagPage.java
@@ -1,19 +1,20 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Comment;
 import model.Hashtag;
 import services.HashtagService;
 //import services.CommentService;
 //import services.UserService;
 
 @SuppressWarnings("serial")
-public class HashtagPage extends HttpServlet{
+public class HashtagPage extends AbstractController {
 
 	//private CommentService commentService = CommentService.getInstance();
 	//private UserService userService = UserService.getInstance();
@@ -25,7 +26,11 @@ public class HashtagPage extends HttpServlet{
 		Hashtag hashtag = hashtagService.getHashtag(hashtagStr);
 		//req.setAttribute("user", hashtag.getAuthor());
 		req.setAttribute("tag",hashtag);
-		req.setAttribute("comments", hashtagService.getComments(hashtagStr));
+		List<Comment> comments = hashtagService.getComments(hashtagStr);
+		for (Comment comment : comments) {
+			comment.setComment(getProcessedComment(comment.getComment()));
+		}
+		req.setAttribute("comments", comments);
 		req.getRequestDispatcher("/WEB-INF/jsp/tag.jsp").forward(req, resp);
 	}
 }
