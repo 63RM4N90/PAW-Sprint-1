b0196f92b8bbaffcfe1f58003d7733590f8ca8b3
SKOLZ <gabriel.zanzotti@gmail.com>
2013-10-07 20:49:17 -0300

    parte del userController hecho. El login en post y get esta implementado, sin testear

--- Sprint2/src/main/java/ar/edu/itba/it/paw/controller/UserController.java
+++ Sprint2/src/main/java/ar/edu/itba/it/paw/controller/UserController.java
@@ -1,46 +1,68 @@
 package ar.edu.itba.it.paw.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.itba.it.paw.model.RankedHashtag;
+import ar.edu.itba.it.paw.model.User;
 import ar.edu.itba.it.paw.services.HashtagService;
 import ar.edu.itba.it.paw.services.UserService;
 
 @Controller
-public class LoginController {
+public class UserController {
 
 	private UserService userService;
 	private HashtagService hashtagService;
-	
+
 	@Autowired
-	public LoginController(UserService userService, HashtagService hashtagService) {
+	public UserController(UserService userService, HashtagService hashtagService) {
 		this.userService = userService;
 		this.hashtagService = hashtagService;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView welcome() {
+	public ModelAndView login(HttpSession s) {
 		ModelAndView mav = new ModelAndView();
-		return mav;
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView login() {
-		ModelAndView mav = new ModelAndView();
+		if (s != null) {
+			mav.setViewName("redirect:profile");
+		}
+		showTopTenHashtags(mav);
 		return mav;
 	}
 
-	/*@Override
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView login(@RequestParam(value = "username", required = true) String username,
+								@RequestParam(value = "password", required = true) String password, HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		User user = userService.authenticate(username, password);
+		if (user != null) {
+			session.setAttribute("userId", user.getId());
+			mav.setViewName("redirect:profile");
+		} else {
+			mav.addObject("username", username);
+			mav.addObject("error", "Invalid user or password.");
+			mav.setViewName("login");
+		}
+		return mav;
+	}
+
+	/*
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		User user = (User) req.getSession().getAttribute("user");
 		if (user == null) {
 			showTopTenHashtags(req);
 			getServletContext().getRequestDispatcher("/WEB-INF/jsp/login.jsp")
-			.forward(req, resp);
+					.forward(req, resp);
 		} else {
 			resp.sendRedirect("profile?user=" + user.getUsername());
 		}
@@ -63,21 +85,22 @@ public class LoginController {
 					resp);
 		}
 	}
-
-	private void showTopTenHashtags(HttpServletRequest req) {
+	*/
+	
+	private void showTopTenHashtags(ModelAndView mav) {
 		List<RankedHashtag> top10;
 
-		if (req.getParameter("period") == null) {
+		if (mav.getModel().get("period") == null) {
 			top10 = hashtagService.topHashtags(30);
 		} else {
-			top10 = hashtagService.topHashtags(Integer.valueOf(req
-					.getParameter("period")));
+			top10 = hashtagService.topHashtags(Integer.valueOf((String) mav
+					.getModel().get("period")));
 		}
 
 		boolean isempty = top10.size() == 0;
-		req.setAttribute("previous", "login");
+		mav.addObject("previous", "login");
 
-		req.setAttribute("ranking", top10);
-		req.setAttribute("isempty", isempty);
-	}*/
+		mav.addObject("ranking", top10);
+		mav.addObject("isempty", isempty);
+	}
 }
