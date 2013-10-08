7119cb0894fb57f8d11ae13cd9569a35e2e3a5ef
German Romarion <grromarion@hotmail.com>
2013-10-08 11:08:17 -0300

    mas migracion spring

--- Sprint2/src/main/java/ar/edu/itba/it/paw/controller/UserController.java
+++ Sprint2/src/main/java/ar/edu/itba/it/paw/controller/UserController.java
@@ -1,7 +1,10 @@
 package ar.edu.itba.it.paw.controller;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.itba.it.paw.model.Comment;
 import ar.edu.itba.it.paw.model.RankedHashtag;
 import ar.edu.itba.it.paw.model.User;
+import ar.edu.itba.it.paw.services.CommentService;
 import ar.edu.itba.it.paw.services.HashtagService;
 import ar.edu.itba.it.paw.services.UserService;
 
@@ -21,11 +26,14 @@ public class UserController {
 
 	private UserService userService;
 	private HashtagService hashtagService;
+	private CommentService commentService;
 
 	@Autowired
-	public UserController(UserService userService, HashtagService hashtagService) {
+	public UserController(UserService userService,
+			HashtagService hashtagService, CommentService commentService) {
 		this.userService = userService;
 		this.hashtagService = hashtagService;
+		this.commentService = commentService;
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
@@ -39,8 +47,10 @@ public class UserController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView login(@RequestParam(value = "username", required = true) String username,
-								@RequestParam(value = "password", required = true) String password, HttpSession session) {
+	public ModelAndView login(
+			@RequestParam(value = "username", required = true) String username,
+			@RequestParam(value = "password", required = true) String password,
+			HttpSession session) {
 		ModelAndView mav = new ModelAndView();
 		User user = userService.authenticate(username, password);
 		if (user != null) {
@@ -53,46 +63,95 @@ public class UserController {
 		}
 		return mav;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView registration() {
 		ModelAndView mav = new ModelAndView();
 		return mav;
 	}
 
-	/*
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		User user = (User) req.getSession().getAttribute("user");
-		if (user == null) {
-			showTopTenHashtags(req);
-			getServletContext().getRequestDispatcher("/WEB-INF/jsp/login.jsp")
-					.forward(req, resp);
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView profile(
+			@RequestParam(value = "user", required = false) String username,
+			@RequestParam(value = "period", required = false) Integer period,
+			HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		User userSession = (User) session.getAttribute("user");
+
+		List<RankedHashtag> top10;
+
+		if (period == null) {
+			top10 = hashtagService.topHashtags(30);
 		} else {
-			resp.sendRedirect("profile?user=" + user.getUsername());
+			top10 = hashtagService.topHashtags(period);
 		}
+		// req.setAttribute("previous", "profile");
+		// req.setAttribute("ranking", top10);
+
+		if (username == null) {
+			if (userSession != null) {
+				if (period == null) {
+					period = 30;
+				}
+				mav.setViewName("redirect:profile?user="
+						+ userSession.getUsername() + "&period=" + period);
+			} else {
+				mav.setViewName("login");
+			}
+		} else {
+			User profile = userService.getUser(username);
+			if (profile != null) {
+				if (userSession != null) {
+					if (profile.getUsername().equals(userSession.getUsername())) {
+						mav.addObject("isOwner", true);
+					}
+				}
+				session.setAttribute("user", profile);
+				mav.addObject("isEmptyPicture", profile.getPicture() == null);
+				List<Comment> comments = commentService.getComments(profile);
+				for (Comment comment : comments) {
+					comment.setComment(getProcessedComment(comment.getComment()));
+				}
+				mav.addObject("comments", comments);
+			} else {
+				mav.setViewName("redirect:profile");
+				return mav;
+			}
+		}
+		return mav;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView editProfile(HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		User userSession = (User) session.getAttribute("user");
+		mav.addObject("sessionUser", userSession);
+		setDefaults(mav, userSession);
+		return mav;
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		showTopTenHashtags(req);
-		String username = req.getParameter("username");
-		String password = req.getParameter("password");
-		User user = userService.authenticate(username, password);
-		if (user != null) {
-			req.getSession().setAttribute("user", user);
-			resp.sendRedirect("profile?user=" + user.getUsername());
-		} else {
-			req.setAttribute("username", username);
-			req.setAttribute("error", "Invalid user or password.");
-			req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req,
-					resp);
-		}
-	}
-	*/
-	
+	/*
+	 * @Override protected void doGet(HttpServletRequest req,
+	 * HttpServletResponse resp) throws ServletException, IOException { User
+	 * user = (User) req.getSession().getAttribute("user"); if (user == null) {
+	 * showTopTenHashtags(req);
+	 * getServletContext().getRequestDispatcher("/WEB-INF/jsp/login.jsp")
+	 * .forward(req, resp); } else { resp.sendRedirect("profile?user=" +
+	 * user.getUsername()); } }
+	 * 
+	 * @Override protected void doPost(HttpServletRequest req,
+	 * HttpServletResponse resp) throws ServletException, IOException {
+	 * showTopTenHashtags(req); String username = req.getParameter("username");
+	 * String password = req.getParameter("password"); User user =
+	 * userService.authenticate(username, password); if (user != null) {
+	 * req.getSession().setAttribute("user", user);
+	 * resp.sendRedirect("profile?user=" + user.getUsername()); } else {
+	 * req.setAttribute("username", username); req.setAttribute("error",
+	 * "Invalid user or password.");
+	 * req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, resp); }
+	 * }
+	 */
+
 	private void showTopTenHashtags(ModelAndView mav) {
 		List<RankedHashtag> top10;
 
@@ -108,5 +167,42 @@ public class UserController {
 
 		mav.addObject("ranking", top10);
 		mav.addObject("isempty", isempty);
+	}
+
+	public String getProcessedComment(String comment) {
+		// Search for URLs
+		if (comment != null && comment.contains("http:")) {
+			int indexOfHttp = comment.indexOf("http:");
+			int endPoint = (comment.indexOf(' ', indexOfHttp) != -1) ? comment
+					.indexOf(' ', indexOfHttp) : comment.length();
+			String url = comment.substring(indexOfHttp, endPoint);
+			String targetUrlHtml = "<a href='" + url + "' target='_blank'>"
+					+ url + "</a>";
+			comment = comment.replace(url, targetUrlHtml);
+		}
+
+		String patternStr = "#([A-Za-z0-9_]+)";
+		Pattern pattern = Pattern.compile(patternStr);
+		Matcher matcher = pattern.matcher(comment);
+		String result = "";
+
+		// Search for Hashtags
+		while (matcher.find()) {
+			result = matcher.group();
+			result = result.replace(" ", "");
+			String search = result.replace("#", "");
+			String searchHTML = "<a href='./hashtag?tag=" + search + "'>"
+					+ result + "</a>";
+			comment = comment.replace(result, searchHTML);
+		}
+		return comment;
+	}
+	
+	private void setDefaults(ModelAndView mav, User original) {
+		mav.addObject("name", original.getName());
+		mav.addObject("surname", original.getSurname());
+		mav.addObject("password", original.getPassword());
+		mav.addObject("confirm", original.getPassword());
+		mav.addObject("description", original.getDescription());
 	}
 }
