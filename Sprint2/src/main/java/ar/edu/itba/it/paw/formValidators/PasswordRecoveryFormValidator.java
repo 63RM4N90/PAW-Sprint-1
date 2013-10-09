ed480c1fc7b7dd7a0d9221f936a31e997cd435fc
German Romarion <grromarion@hotmail.com>
2013-10-09 01:01:10 -0300

    form validator para la password recovery escrito

--- Sprint2/src/main/java/ar/edu/itba/it/paw/formValidators/PasswordRecoveryFormValidator.java
+++ Sprint2/src/main/java/ar/edu/itba/it/paw/formValidators/PasswordRecoveryFormValidator.java
@@ -1,5 +1,71 @@
 package ar.edu.itba.it.paw.formValidators;
 
-public class PasswordRecoveryFormValidator {
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
+import ar.edu.itba.it.paw.model.User;
+
+public class PasswordRecoveryFormValidator implements Validator {
+
+	private static final int MAX_STRING_LENGTH = 32;
+	private static final int MIN_PASSWORD_LENGTH = 8;
+	private static final int MAX_PASSWORD_LENGTH = 16;
+	private static final int MAX_QUE_AND_ANS_LENGTH = 64;
+	
+	private String secretAnswer;
+	private String password;
+	private String confirmPassword;
+	private String userToRecover;
+	
+	public PasswordRecoveryFormValidator(String secretAnswer, String password,
+			String confirmPassword, String userToRecover) {
+		this.secretAnswer = secretAnswer;
+		this.password = password;
+		this.confirmPassword = confirmPassword;
+		this.userToRecover = userToRecover;
+	}
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return User.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		checkLength(errors);
+		checkEmpt(errors);
+	}
+	
+	private void checkEmpt(Errors errors) {
+		if (userToRecover == null) {
+			errors.rejectValue("username", "empty.username");
+		}
+		if (password == null) {
+			errors.rejectValue("password", "empty.password");
+		}
+		if (confirmPassword == null) {
+			errors.rejectValue("confirmPassword", "empty.confirmPassword");
+		}
+		if (secretAnswer == null) {
+			errors.rejectValue("secretAnswer", "empty.secretAnswer");
+		}
+	}
+	
+	private void checkLength(Errors errors) {
+		if (userToRecover.length() > MAX_STRING_LENGTH || userToRecover.length() == 0) {
+			errors.rejectValue("username", "length.username");
+		}
+		if (password.length() < MIN_PASSWORD_LENGTH
+				|| password.length() > MAX_PASSWORD_LENGTH) {
+			errors.rejectValue("password", "length.password");
+		}
+		if (confirmPassword.length() < MIN_PASSWORD_LENGTH
+				|| confirmPassword.length() > MAX_PASSWORD_LENGTH) {
+			errors.rejectValue("confirmPassword", "length.confirmPassword");
+		}
+		if (secretAnswer.length() > MAX_QUE_AND_ANS_LENGTH
+				|| secretAnswer.length() == 0) {
+			errors.rejectValue("secretAnswer", "length.secretAnswer");
+		}
+	}
 }
