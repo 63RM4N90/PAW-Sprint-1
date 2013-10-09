6d62f81d4c151a5eb27330fa6ef5d6bf908cdf91
German Romarion <grromarion@hotmail.com>
2013-10-09 00:26:04 -0300

    validator para la registracion del usuario implementado

--- Sprint2/src/main/java/ar/edu/itba/it/paw/converters/StringToIntegerConverter.java
+++ Sprint2/src/main/java/ar/edu/itba/it/paw/converters/StringToIntegerConverter.java
@@ -1,7 +1,9 @@
 package ar.edu.itba.it.paw.converters;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StringToIntegerConverter implements Converter<String, Integer> {
 
 	@Override
