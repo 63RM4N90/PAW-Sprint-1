04d63a2394be30eef2b92c1170c05f0c0da2259b
German Romarion <grromarion@hotmail.com>
2013-10-11 11:01:51 -0300

    paquetes borrados

--- Sprint2/src/main/java/ar/edu/itba/it/paw/controller/UserController.java
+++ Sprint2/src/main/java/ar/edu/itba/it/paw/controller/UserController.java
@@ -1,6 +1,5 @@
 package ar.edu.itba.it.paw.controller;
 
-import java.net.BindException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,18 +8,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import ar.edu.itba.it.paw.form.UserForm;
 import ar.edu.itba.it.paw.formValidators.PasswordRecoveryFormValidator;
-import ar.edu.itba.it.paw.formValidators.UserFormValidator;
 import ar.edu.itba.it.paw.model.Comment;
 import ar.edu.itba.it.paw.model.RankedHashtag;
 import ar.edu.itba.it.paw.model.User;
