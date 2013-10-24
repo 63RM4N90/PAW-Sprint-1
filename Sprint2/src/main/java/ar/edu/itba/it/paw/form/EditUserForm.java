64a4823d3af28bc327d3c0554b36681bbb0b6de2
German Romarion <grromarion@hotmail.com>
2013-10-23 22:20:19 -0300

    form validator para la edición de datos de un usuario

---
+++ Sprint2/src/main/java/ar/edu/itba/it/paw/form/EditUserForm.java
@@ -0,0 +1,110 @@
+package ar.edu.itba.it.paw.form;
+
+import java.util.Date;
+
+import ar.edu.itba.it.paw.model.User;
+
+public class EditUserForm {
+
+	private User user;
+	private String name;
+	private String surname;
+	private String password;
+	private String confirmPassword;
+	private String description;
+	private String secretQuestion;
+	private String secretAnswer;
+
+	public EditUserForm() {
+	}
+
+	public EditUserForm(User user) {
+		this.user = user;
+		this.name = user.getName();
+		this.surname = user.getSurname();
+		this.password = user.getPassword();
+		this.description = user.getDescription();
+		this.secretQuestion = user.getSecretQuestion();
+		this.secretAnswer = user.getSecretAnswer();
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSecretQuestion() {
+		return secretQuestion;
+	}
+
+	public void setSecretQuestion(String secretQuestion) {
+		this.secretQuestion = secretQuestion;
+	}
+
+	public String getSecretAnswer() {
+		return secretAnswer;
+	}
+
+	public void setSecretAnswer(String secretAnswer) {
+		this.secretAnswer = secretAnswer;
+	}
+
+	public User build() {
+		if (user == null) {
+			return new User(name, surname, user.getUsername(), description,
+					confirmPassword, null, secretQuestion, secretAnswer,
+					new Date());
+		} else {
+			user.setName(name);
+			user.setSurname(surname);
+			user.setPassword(password);
+			user.setDescription(description);
+			user.setSecretQuestion(secretQuestion);
+			user.setSecretAnswer(secretAnswer);
+			return user;
+		}
+	}
+}
---
+++ Sprint2/src/main/java/ar/edu/itba/it/paw/formValidators/EditUserFormValidator.java
@@ -0,0 +1,93 @@
+package ar.edu.itba.it.paw.formValidators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import ar.edu.itba.it.paw.form.EditUserForm;
+
+public class EditUserFormValidator implements Validator {
+
+	private static final int MAX_STRING_LENGTH = 32;
+	private static final int MIN_PASSWORD_LENGTH = 8;
+	private static final int MAX_PASSWORD_LENGTH = 16;
+	private static final int MAX_DESCRIPTION_LENGTH = 140;
+	private static final int MAX_QUE_AND_ANS_LENGTH = 64;
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return EditUserForm.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		EditUserForm userForm = (EditUserForm) target;
+		checkEmptyInputs(userForm, errors);
+		checkInputLength(userForm, errors);
+		if (userForm.getPassword().length() > MIN_PASSWORD_LENGTH
+				&& userForm.getPassword().length() < MAX_PASSWORD_LENGTH) {
+			checkPasswordConfirm(userForm, errors);
+		}
+	}
+
+	private void checkPasswordConfirm(EditUserForm editUserForm, Errors errors) {
+		if (!editUserForm.getPassword().equals(
+				editUserForm.getConfirmPassword())) {
+			errors.rejectValue("password", "nonmatch");
+		}
+	}
+
+	private void checkInputLength(EditUserForm editUserForm, Errors errors) {
+		if (editUserForm.getName().length() > MAX_STRING_LENGTH
+				|| editUserForm.getName().length() == 0) {
+			errors.rejectValue("name", "length");
+		}
+		if (editUserForm.getSurname().length() > MAX_STRING_LENGTH
+				|| editUserForm.getSurname().length() == 0) {
+			errors.rejectValue("surname", "length");
+		}
+		if (editUserForm.getPassword().length() < MIN_PASSWORD_LENGTH
+				|| editUserForm.getPassword().length() > MAX_PASSWORD_LENGTH) {
+			errors.rejectValue("password", "length");
+		}
+		if (editUserForm.getConfirmPassword().length() < MIN_PASSWORD_LENGTH
+				|| editUserForm.getConfirmPassword().length() > MAX_PASSWORD_LENGTH) {
+			errors.rejectValue("confirmPassword", "length");
+		}
+		if (editUserForm.getDescription().length() > MAX_DESCRIPTION_LENGTH
+				|| editUserForm.getDescription().length() == 0) {
+			errors.rejectValue("description", "length");
+		}
+		if (editUserForm.getSecretQuestion().length() > MAX_QUE_AND_ANS_LENGTH
+				|| editUserForm.getSecretQuestion().length() == 0) {
+			errors.rejectValue("secretQuestion", "length");
+		}
+		if (editUserForm.getSecretAnswer().length() > MAX_QUE_AND_ANS_LENGTH
+				|| editUserForm.getSecretAnswer().length() == 0) {
+			errors.rejectValue("secretAnswer", "length");
+		}
+	}
+
+	private void checkEmptyInputs(EditUserForm editUserForm, Errors errors) {
+		if (editUserForm.getName() == null) {
+			errors.rejectValue("name", "empty");
+		}
+		if (editUserForm.getSurname() == null) {
+			errors.rejectValue("surname", "empty");
+		}
+		if (editUserForm.getPassword() == null) {
+			errors.rejectValue("password", "empty");
+		}
+		if (editUserForm.getConfirmPassword() == null) {
+			errors.rejectValue("confirmPassword", "empty");
+		}
+		if (editUserForm.getDescription() == null) {
+			errors.rejectValue("description", "empty");
+		}
+		if (editUserForm.getSecretQuestion() == null) {
+			errors.rejectValue("secretQuestion", "empty");
+		}
+		if (editUserForm.getSecretAnswer() == null) {
+			errors.rejectValue("secretAnswer", "empty");
+		}
+	}
+}
