ffe4a7b6e53d7efd49e515af488e7119fa860f0e
German Romarion <grromarion@hotmail.com>
2014-08-02 13:32:12 -0300

    Created user actions panel.

--- Sprint3/src/main/java/ar/edu/itba/it/paw/web/user/ProfilePage.java
+++ Sprint3/src/main/java/ar/edu/itba/it/paw/web/user/ProfilePage.java
@@ -1,14 +1,11 @@
 package ar.edu.itba.it.paw.web.user;
 
-import java.io.IOException;
 import java.util.Date;
-import java.util.InvalidPropertiesFormatException;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.image.Image;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -17,8 +14,6 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import ar.edu.itba.it.paw.domain.Comment;
 import ar.edu.itba.it.paw.domain.CommentRepo;
-import ar.edu.itba.it.paw.domain.EntityModel;
-import ar.edu.itba.it.paw.domain.Notification;
 import ar.edu.itba.it.paw.domain.NotificationRepo;
 import ar.edu.itba.it.paw.domain.User;
 import ar.edu.itba.it.paw.domain.UserRepo;
@@ -39,7 +34,6 @@ public class ProfilePage extends BasePage {
 	private String commentTextarea;
 	private transient User currentUser;
 
-	@SuppressWarnings("serial")
 	public ProfilePage(final PageParameters parameters) {
 		currentUser = users.getUser(parameters.get("username").toString());
 		
@@ -47,8 +41,6 @@ public class ProfilePage extends BasePage {
 			setResponsePage(getApplication().getHomePage());
 			return;
 		}
-
-		boolean isFollowing = loggedUserIsFollowing();
 
 		boolean isSameUser = loggedUserIsCurrentUser();
 
@@ -63,95 +55,8 @@ public class ProfilePage extends BasePage {
 		add(new Label("profileSurname", currentUser.getSurname()));
 		add(new Label("profileDescription", currentUser.getDescription()));
 		add(new Label("profileVisits", currentUser.getVisits()));
-		add(new Link<String>("followersLink") {
-
-			@Override
-			public void onClick() {
-				setResponsePage(new FollowersPage(currentUser.getId()));
-			}
-		}.add(new Label("followersAmount", currentUser.getFollowersAmount())));
 		
-		add(new Link<String>("followingLink") {
-
-			@Override
-			public void onClick() {
-				setResponsePage(new FollowingPage(currentUser.getId()));
-			}
-		}.add(new Label("followingAmount", currentUser.getFollowingAmount())));
-
-		add(new Link<String>("notificationsLink") {
-
-			@Override
-			public void onClick() {
-				setResponsePage(new NotificationsPage());
-			}
-		}.add(new Label("notifications", currentUser.getNotificationsAmount()))
-				.setVisible(isSameUser));
-
-		add(new Link<String>("suggestedUsersLink") {
-
-			@Override
-			public void onClick() {
-				try {
-					setResponsePage(new SuggestedFriendsPage());
-				} catch (InvalidPropertiesFormatException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}.setVisible(isSameUser));
-
-		add(new Link<String>("favouritesLink") {
-
-			@Override
-			public void onClick() {
-				setResponsePage(new FavouritesPage());
-			}
-		}.setVisible(SocialCthulhuSession.get().getUsername()
-				.equals(users.getUser(currentUser.getId()).getUsername())));
-
-		add(new Link<String>("editProfileLink") {
-
-			@Override
-			public void onClick() {
-				setResponsePage(new EditProfilePage(
-						users.getUser(SocialCthulhuSession.get().getUsername())));
-			}
-		}.add(new Label("edit", getString("edit"))).setVisible(isSameUser));
-
-		add(new Link<User>("followLink", new EntityModel<User>(User.class,
-				currentUser.getId())) {
-
-			@Override
-			public void onClick() {
-				User user = users.getUser(SocialCthulhuSession.get()
-						.getUsername());
-				Notification notification = new Notification(user,
-						user.getUsername() + " is following you :).");
-				notifications.save(notification);
-				user.follow(getModelObject(), notification);
-				setResponsePage(new ProfilePage(new PageParameters().set(
-						"username", getModelObject().getUsername())));
-			}
-		}.add(new Label("follow", getString("follow")).setVisible(!isFollowing
-				&& !isSameUser)));
-
-		add(new Link<User>("unfollowLink", new EntityModel<User>(User.class,
-				currentUser.getId())) {
-
-			@Override
-			public void onClick() {
-				User user = users.getUser(SocialCthulhuSession.get()
-						.getUsername());
-				user.unfollow(getModelObject());
-				setResponsePage(new ProfilePage(new PageParameters().set(
-						"username", getModelObject().getUsername())));
-			}
-		}.add(new Label("unfollow", getString("unfollow"))
-				.setVisible(isFollowing)));
+		add(new UserActionsPanel("user_actions_panel", currentUser, isSameUser));
 
 		add(new FeedbackPanel("errorPanel").setVisible(isSameUser));
 
@@ -176,11 +81,6 @@ public class ProfilePage extends BasePage {
 
 		add(new CommentsPanel("comments-panel", currentUser.getId(),
 				currentUser.getComments()));
-	}
-
-	private boolean loggedUserIsFollowing() {
-		return users.getUser(SocialCthulhuSession.get().getUsername())
-				.getFollowing().contains(currentUser);
 	}
 
 	private boolean loggedUserIsCurrentUser() {
---
+++ Sprint3/src/main/java/ar/edu/itba/it/paw/web/user/UserActionsPanel.java
@@ -0,0 +1,130 @@
+package ar.edu.itba.it.paw.web.user;
+
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import ar.edu.itba.it.paw.domain.EntityModel;
+import ar.edu.itba.it.paw.domain.Notification;
+import ar.edu.itba.it.paw.domain.NotificationRepo;
+import ar.edu.itba.it.paw.domain.User;
+import ar.edu.itba.it.paw.domain.UserRepo;
+import ar.edu.itba.it.paw.web.SocialCthulhuSession;
+
+public class UserActionsPanel extends Panel {
+
+	private static final long serialVersionUID = -4235379871556971356L;
+	private transient User currentUser;
+	@SpringBean
+	private UserRepo users;
+	@SpringBean
+	private NotificationRepo notifications;
+	
+	@SuppressWarnings("serial")
+	public UserActionsPanel(String id, User userPage, final boolean isSameUser) {
+		super(id);
+		this.currentUser = userPage;
+		
+		boolean isFollowing = loggedUserIsFollowing();
+		
+		add(new Link<String>("followersLink") {
+
+			@Override
+			public void onClick() {
+				setResponsePage(new FollowersPage(currentUser.getId()));
+			}
+		}.add(new Label("followersAmount", currentUser.getFollowersAmount())));
+		
+		add(new Link<String>("followingLink") {
+
+			@Override
+			public void onClick() {
+				setResponsePage(new FollowingPage(currentUser.getId()));
+			}
+		}.add(new Label("followingAmount", currentUser.getFollowingAmount())));
+
+		add(new Link<String>("notificationsLink") {
+
+			@Override
+			public void onClick() {
+				setResponsePage(new NotificationsPage());
+			}
+		}.add(new Label("notifications", currentUser.getNotificationsAmount()))
+				.setVisible(isSameUser));
+
+		add(new Link<String>("suggestedUsersLink") {
+
+			@Override
+			public void onClick() {
+				try {
+					setResponsePage(new SuggestedFriendsPage());
+				} catch (InvalidPropertiesFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}.setVisible(isSameUser));
+
+		add(new Link<String>("favouritesLink") {
+
+			@Override
+			public void onClick() {
+				setResponsePage(new FavouritesPage());
+			}
+		}.setVisible(SocialCthulhuSession.get().getUsername()
+				.equals(users.getUser(currentUser.getId()).getUsername())));
+
+		add(new Link<String>("editProfileLink") {
+
+			@Override
+			public void onClick() {
+				setResponsePage(new EditProfilePage(
+						users.getUser(SocialCthulhuSession.get().getUsername())));
+			}
+		}.add(new Label("edit", getString("edit"))).setVisible(isSameUser));
+
+		add(new Link<User>("followLink", new EntityModel<User>(User.class,
+				currentUser.getId())) {
+
+			@Override
+			public void onClick() {
+				User user = users.getUser(SocialCthulhuSession.get()
+						.getUsername());
+				Notification notification = new Notification(user,
+						user.getUsername() + " is following you :).");
+				notifications.save(notification);
+				user.follow(getModelObject(), notification);
+				setResponsePage(new ProfilePage(new PageParameters().set(
+						"username", getModelObject().getUsername())));
+			}
+		}.add(new Label("follow", getString("follow")).setVisible(!isFollowing
+				&& !isSameUser)));
+
+		add(new Link<User>("unfollowLink", new EntityModel<User>(User.class,
+				currentUser.getId())) {
+
+			@Override
+			public void onClick() {
+				User user = users.getUser(SocialCthulhuSession.get()
+						.getUsername());
+				user.unfollow(getModelObject());
+				setResponsePage(new ProfilePage(new PageParameters().set(
+						"username", getModelObject().getUsername())));
+			}
+		}.add(new Label("unfollow", getString("unfollow"))
+				.setVisible(isFollowing)));
+	}
+	
+	private boolean loggedUserIsFollowing() {
+		return users.getUser(SocialCthulhuSession.get().getUsername())
+				.getFollowing().contains(currentUser);
+	}
+}
